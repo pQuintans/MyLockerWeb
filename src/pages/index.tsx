e678e7eb54e6eb8cdd5c5a86228313f754093c32
pQuintans <caixote.dom@gmail.com>
2022-11-23 22:36:05 -0300

    Configure ESLint && Install and setup tailwind and postcss

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='flex items-center justify-center w-screen h-screen  font-bold bg-slate-600'>
+      <p className='text-gray-200 text-5xl'>Home</p>
+    </div>
+  )
+}
